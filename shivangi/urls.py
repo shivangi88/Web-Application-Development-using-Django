152d3b30abda49ec6e14c171a8453ace2f524d61
Shivangi Tripathi <shivangitripathi17@gmail.com>
2019-11-05 13:49:52 -0500

    Update Done

--- shivangi/urls.py
+++ shivangi/urls.py
@@ -18,8 +18,9 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    path('travello',include('travello.urls')),
+    path('travello/',include('travello.urls')),
     path('admin/', admin.site.urls),
+    path('travello/accounts/',include('accounts.urls')),
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
